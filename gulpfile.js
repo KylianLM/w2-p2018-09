7bddf663523fc0599d562f9af783afc602d2d61a
Kylian LE METTE <kylian.le.mette@gmail.com>
2017-02-23 10:30:27 +0100

    [ADD] CLEAN CSS > BASE DIRECTORY > config gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+stylus = require('gulp-stylus'),
+postcss = require('gulp-postcss'),
+lost = require('lost'),
+autoprefixer = require('autoprefixer'),
+sourcemaps = require('gulp-sourcemaps'),
+browser_sync = require('browser-sync'),
+cleanCSS = require('gulp-clean-css'),
+reload = browser_sync.reload;
+
+const src = "./src/"
+const dist = "./dist/";
+
+gulp.task('stylus', function() {
+	return gulp.src( src + './assets/stylus/**/*.styl')
+	.pipe(sourcemaps.init())
+	.pipe(stylus())
+	.pipe(postcss([
+		autoprefixer(),
+		cleanCSS()
+		]))
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest( dist + './assets/css'))
+	.pipe(reload({ stream:true }));
+});
+
+gulp.task('serve', function() {
+	browser_sync({
+		server: {
+			baseDir: './dist'
+		}
+	});
+	gulp.watch([src + './assets/stylus/**/*.styl'], ['stylus']);
+});
