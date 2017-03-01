d28854cebac94853ce20a98549554763010d88f4
Kylian LE METTE <kylian.le.mette@gmail.com>
2017-03-01 11:52:40 +0100

    [ADD] Login Modal > Modal CSS > Input CSS > Colors stylus

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -22,24 +22,47 @@ function getXMLHttpRequest() {
 	return xhr;
 }
 
-function init() {
-    navigator.geolocation.getCurrentPosition(function(pos) {
-        var crd = pos.coords;
-            map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: crd.latitude, lng: crd.longitude},
-            zoom: 15
-        });
-    });
-    var xhr = getXMLHttpRequest();
+// Modal Js
+var modalLogin = document.getElementById('modalLogin'),
+	modalRegister = document.getElementById('modalRegister'),
+	btnRegister = document.getElementById('openModalRegister'),
+	btnLogin = document.getElementById('openModalLogin'),
+	closeLogin = modalLogin.querySelector('.close'),
+	closeRegister = modalRegister.querySelector('.close');
 
-    xhr.onreadystatechange = function() {
+btnLogin.addEventListener('click', function () {
+	modalLogin.style.display = "block";
+});
+btnRegister.addEventListener('click', function () {
+	modalRegister.style.display = "block";
+});
+
+closeLogin.addEventListener('click', function () {
+	modalLogin.style.display =  "none";
+})
+closeRegister.addEventListener('click', function () {
+	modalRegister.style.display =  "none";
+})
+
+// Init Google Maps
+function init() {
+	navigator.geolocation.getCurrentPosition(function(pos) {
+		var crd = pos.coords;
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: crd.latitude, lng: crd.longitude},
+			zoom: 15
+		});
+	});
+	var xhr = getXMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
 			console.log(JSON.parse(xhr.responseText));
 		}
 	};
 
-    xhr.open("GET",
-    	"https://opendata.paris.fr/api/records/1.0/search/?dataset=stations-velib-disponibilites-en-temps-reel&lang=fr&facet=banking&facet=bonus&facet=status&facet=contract_name&geofilter.distance=48.9093300219%2C+2.35805056574%2C300",
-    	true);
-    xhr.send(null);
+	xhr.open("GET",
+		"https://opendata.paris.fr/api/records/1.0/search/?dataset=stations-velib-disponibilites-en-temps-reel&lang=fr&facet=banking&facet=bonus&facet=status&facet=contract_name&geofilter.distance=48.9093300219%2C+2.35805056574%2C300",
+		true);
+	xhr.send(null);
 }
